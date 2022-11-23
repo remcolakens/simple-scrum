1ce4d7680f55ad1716fa2c767cb5fc3bc1bb2c03
Remco Lakens <remco.lakens@gmail.com>
2022-11-23 18:01:09 +0100

    add basic grid

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,20 +1,8 @@
-import { screen } from '@testing-library/react';
 import Home from '../pages/index';
 import { renderWithChakra } from './utils';
 
 describe('Home', () => {
-	it('renders a heading', () => {
+	it('renders', () => {
 		renderWithChakra(<Home />);
-
-		expect(
-			screen.getByRole('heading', {
-				name: 'Welcome to your Next.js Boilerplate',
-			})
-		).toBeInTheDocument();
-	});
-
-	it('renders a text row', () => {
-		renderWithChakra(<Home />);
-		expect(screen.getByText('(xs) This is default text!')).toBeInTheDocument();
 	});
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,72 +1,42 @@
-import { Cat } from '@/svgs';
 import {
 	Box,
-	Button,
-	ComponentDefaultProps,
 	Container,
-	Flex,
+	Grid,
+	GridItem,
 	Heading,
-	Progress,
-	Stack,
 	Text,
 } from '@chakra-ui/react';
-import styled from '@emotion/styled';
-import { useIdle } from 'react-use';
-
-const StyledTitle = styled.h1`
-	color: ${(props: ComponentDefaultProps) => props.theme.colors.tertiary};
-	font-weight: bold;
-	text-decoration: underline;
-	font-size: 3rem;
-	text-align: center;
-`;
+import React from 'react';
 
 export const Home: React.FC = () => {
-	const isIdle = useIdle(300);
-
 	return (
 		<Container maxW="container.lg">
-			<StyledTitle>Welcome to your Next.js Boilerplate</StyledTitle>
+			<Grid templateColumns="repeat(4, 1fr)" gap={5}>
+				<GridItem bg="gray.100" p={5} flexBasis={1}>
+					<Heading bg="gray.300" size="md" p={2} mb={2} flexShrink={0}>
+						Good
+					</Heading>
 
-			<Flex mt={20} flexWrap="wrap">
-				<Box color="blue.200" flex={1}>
-					<Cat />
-				</Box>
-
-				<Box flex={1}>
-					<Stack spacing={2}>
-						<Heading textAlign={{ sm: 'center', md: 'left' }}>
-							Default heading
-						</Heading>
-						<Text align={{ sm: 'center', md: 'left' }} fontSize="xl">
-							(xl) This is default text!
-						</Text>
-						<Text align={{ sm: 'center', md: 'left' }} fontSize="lg">
-							(lg) This is default text!
-						</Text>
-						<Text align={{ sm: 'center', md: 'left' }} fontSize="md">
-							(md) This is default text!
-						</Text>
-						<Text align={{ sm: 'center', md: 'left' }} fontSize="sm">
-							(sm) This is default text!
-						</Text>
-						<Text align={{ sm: 'center', md: 'left' }} fontSize="xs">
-							(xs) This is default text!
-						</Text>
-					</Stack>
-					<Flex justifyContent={{ sm: 'center', md: 'left' }}>
-						<Button mt={10} variant="primary">
-							Submit
-						</Button>
-					</Flex>
-				</Box>
-			</Flex>
-
-			<Progress value={80} mt={5} />
-
-			<Text align="right" fontSize="xs" my={5} color="gray.500">
-				User is idle: {isIdle ? 'Yes 😴' : 'Nope'}
-			</Text>
+					<Box p={2} my={2} border="1px" borderColor="gray.700" bg="white">
+						<Text>API finally pushed to prod</Text>
+					</Box>
+				</GridItem>
+				<GridItem bg="gray.100" p={5} flexBasis={1}>
+					<Heading bg="gray.300" size="md" p={2} mb={2} flexShrink={0}>
+						Bad
+					</Heading>
+				</GridItem>
+				<GridItem bg="gray.100" p={5} flexBasis={1}>
+					<Heading bg="gray.300" size="md" p={2} mb={2} flexShrink={0}>
+						Actions
+					</Heading>
+				</GridItem>
+				<GridItem bg="gray.100" p={5} flexBasis={1}>
+					<Heading bg="gray.300" size="md" p={2} mb={2} flexShrink={0}>
+						Ideas
+					</Heading>
+				</GridItem>
+			</Grid>
 		</Container>
 	);
 };
