2cfda2886e00d7205cbd3aaba79a4e7799fe06a8
Remco Lakens <remco.lakens@gmail.com>
2022-11-23 18:06:27 +0100

    add: page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,16 @@ import { primaryTheme } from '@/styles/themes';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Global } from '@emotion/react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Head>
+				<title>Simple Scrum</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+
 			<Global styles={global} />
 			<ChakraProvider resetCSS theme={primaryTheme}>
 				<Component {...pageProps} />
